af06088670532aaa9493d314020f12592c6cec87
Dustopus <dustopus@users.noreply.github.com>
2026-03-25 21:00:46 +0800

    feat: 添加单元测试类和 Dockerfile 多阶段构建

---
+++ src/test/java/com/dustopus/qianyan/QianYanApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dustopus.qianyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QianYanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
